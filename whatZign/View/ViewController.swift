66b5301367691aa1a7e2847c2cd58a7debbafc00
Madison <madison.k.adams@gmail.com>
2023-05-10 16:18:59 -0400

    logic signs

--- whatZign/View/ViewController.swift
+++ whatZign/View/ViewController.swift
@@ -39,11 +39,59 @@ class ViewController: UIViewController {
         
     }
     
-    
-    
     //This will lead to the other screen.
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print("calculate!")
+        
+        let components = datePicker.calendar.dateComponents([.month, .day], from: datePicker.date)
+        let month = components.month
+        let day = components.day
+        
+        let sign : String
+        
+//        Capricorn - December 22 to about January 19 12, 1 22-31st
+//        Aquarius - January 20 to about February 18 1,2
+//        Pisces - February 19 to about March 20 2,3
+//        Aries - March 21 to about April 19 3,4
+//        Taurus - April 20 to about May 20 4,5
+//        Gemini - May 21 to about June 21 5,6
+//        Cancer - June 22 to about July 22 6,7
+//        Leo - July 23 to about August 22 7,6
+//        Virgo - August 23 to about September 22 7,8
+//        Libra - September 22 to about October 23 8,9
+//        Scorpio - October 23-November 21 9,10
+//        Sagittarius - November 22 to about December 21. 11,10
+        
+        //result
+        let capRange1 = 22...31
+        let capRange2 = 1...19
+        let aqRange1 = 20...31
+        let aqRange2 = 1...18
+        
+        //refactor this. 
+        if(month == 12 && capRange1.contains(day!) ){
+            sign = "Capricorn"
+            print(sign)
+        }
+        else if(month == 1 && capRange2.contains(day!)){
+            sign = "Capricorn"
+            print(sign)
+        }else if(month == 1 && aqRange1.contains(day!)){
+            sign = "Aquarius"
+            print(sign)
+        }else if(month == 2 && aqRange2.contains(day!)){
+            sign = "Aquarius"
+            print(sign)
+        }
+        
+        
+ 
+        
+        
+        
+        
+       
+       
+        
     }
     
 }
