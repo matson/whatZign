e1edf031e1a636f06aee6a5a280e7ab62fe0ff2f
Madison <madison.k.adams@gmail.com>
2023-05-11 12:35:54 -0400

    finished sign logic

--- whatZign/View/ViewController.swift
+++ whatZign/View/ViewController.swift
@@ -42,46 +42,86 @@ class ViewController: UIViewController {
     //This will lead to the other screen.
     @IBAction func calculatePressed(_ sender: UIButton) {
         
+        //This will extract the data needed from the date picker:
         let components = datePicker.calendar.dateComponents([.month, .day], from: datePicker.date)
         let month = components.month
         let day = components.day
         
         let sign : String
         
-//        Capricorn - December 22 to about January 19 12, 1 22-31st
-//        Aquarius - January 20 to about February 18 1,2
-//        Pisces - February 19 to about March 20 2,3
-//        Aries - March 21 to about April 19 3,4
-//        Taurus - April 20 to about May 20 4,5
-//        Gemini - May 21 to about June 21 5,6
-//        Cancer - June 22 to about July 22 6,7
-//        Leo - July 23 to about August 22 7,6
-//        Virgo - August 23 to about September 22 7,8
-//        Libra - September 22 to about October 23 8,9
-//        Scorpio - October 23-November 21 9,10
-//        Sagittarius - November 22 to about December 21. 11,10
-        
-        //result
+        //Date Ranges:
         let capRange1 = 22...31
         let capRange2 = 1...19
         let aqRange1 = 20...31
         let aqRange2 = 1...18
+        let pisRange1 = 19...29
+        let pisRange2 = 1...20
+        let ariRange1 = 21...31
+        let ariRange2 = 1...19
+        let tarRange1 = 20...30
+        let tarRange2 = 1...20
+        let gemRange1 = 21...31
+        let gemRange2 = 1...21
+        let canRange1 = 22...30
+        let canRange2 = 1...22
+        let leoRange1 = 23...31
+        let leoRange2 = 1...22
+        let virRange1 = 23...31
+        let virRange2 = 1...22
+        let libRange1 = 23...30
+        let libRange2 = 1...23
+        let scrRange1 = 24...31
+        let scrRange2 = 1...21
         
-        //refactor this.
+        
+        //refactor this if possible...
+        //Logic to determine the signs associated with the dates.
         if(month == 12 && capRange1.contains(day!) || month == 1 && capRange2.contains(day!) ){
             sign = "Capricorn"
             print(sign)
         }
-////        else if(month == 1 && capRange2.contains(day!)){
-////            sign = "Capricorn"
-////            print(sign)
-//        }else if(month == 1 && aqRange1.contains(day!)){
-//            sign = "Aquarius"
-//            print(sign)
-//        }else if(month == 2 && aqRange2.contains(day!)){
-//            sign = "Aquarius"
-//            print(sign)
+        else if(month == 1 && aqRange1.contains(day!) || month == 2 && aqRange2.contains(day!) ){
+            sign = "Aquarius"
+            print(sign)
+        }else if(month == 2 && pisRange1.contains(day!) || month == 3 && pisRange2.contains(day!)){
+            sign = "Pisces"
+            print(sign)
+        }else if(month == 3 && ariRange1.contains(day!) || month == 4 && ariRange2.contains(day!)){
+            sign = "Aries"
+            print(sign)
+        }else if(month == 4 && tarRange1.contains(day!) || month == 5 && tarRange2.contains(day!)){
+            sign = "Taurus"
+            print(sign)
+        }else if(month == 5 && gemRange1.contains(day!) || month == 6 && gemRange2.contains(day!)){
+            sign = "Gemini"
+            print(sign)
+        }else if(month == 6 && canRange1.contains(day!) || month == 7 && canRange2.contains(day!)){
+            sign = "Cancer"
+            print(sign)
+        }else if(month == 7 && leoRange1.contains(day!) || month == 8 && leoRange2.contains(day!)){
+            sign = "Leo"
+            print(sign)
+        }else if(month == 8 && virRange1.contains(day!) || month == 9 && virRange2.contains(day!)){
+            sign = "Virgo"
+            print(sign)
+        }else if(month == 9 && libRange1.contains(day!) || month == 10 && libRange2.contains(day!)){
+            sign = "Libra"
+            print(sign)
+        }else if(month == 10 && scrRange1.contains(day!) || month == 11 && scrRange2.contains(day!)){
+            sign = "Scorpio"
+            print(sign)
+        }else{
+            
+            sign = "Sagittarius"
+            print(sign)
+        }
+        
+        
+//        func calculateSign(month: Int, day: Int) -> String{
+//
+//            return sign
 //        }
+        
         
         
  
